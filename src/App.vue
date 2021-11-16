d47c98939d2fc0312674788dd154ffbdb57b5ab2
Stathis Ioannidis <56734272+Stathis96@users.noreply.github.com>
2021-11-16 16:50:54 +0200

    Skeleton of proj

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="h-screen"> 
+  <Header/>
+  <div class="h-5/6 w-full bg-base-300">
+    <MainTable/>
+    <Modal/>
+    <DeleteModal/>
+  </div>
+
+  <Footer/>
+  </div>
+</template>
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import MainTable from './components/MainTable.vue'
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Modal from './components/Modal.vue'
+import DeleteModal from './components/DeleteModal.vue'
+
+</script>
+
+
+<style>
+
+</style>
