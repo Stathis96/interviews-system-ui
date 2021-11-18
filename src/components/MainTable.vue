1cd24e25245739788049201af8afec24bb378a9f
Stathis Ioannidis <56734272+Stathis96@users.noreply.github.com>
2021-11-18 16:52:06 +0200

    Main table updated

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -6,9 +6,9 @@
 
                     <div class="flex items-center relative">
                       <!-- Search Input -->
-                      <input class="border-2 border-neutral bg-red transition h-12 px-5 pr-16 rounded-md focus:outline-none w-full text-black text-lg " type="search" name="search" placeholder="Search" />
+                      <input class="border-2 border-neutral bg-red transition h-10 px-5 pr-16 rounded-md focus:outline-none w-full text-black text-lg " type="search" name="search" placeholder="Search" />
                       <button type="submit" class="absolute right-2 top-3 mr-4">
-                        <svg class="text-black h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
+                        <svg class="text-black h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
                           <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
                         </svg>
                       </button>
@@ -51,12 +51,16 @@
                     <div class="lg:ml-6 flex items-center">
                     <!-- Button for Showing Interviews with NULL RESULT -->
                         <button class="bg-gray-200 transition duration-150 ease-in-out focus:outline-none border border-transparent focus:border-gray-800 focus:shadow-outline-gray hover:bg-gray-300 rounded text-indigo-700 px-5 h-8 flex items-center text-sm">Interviews with Null Result</button>
+                        <!-- ADD NEW INTERVIEW -->
                         <div class="text-white ml-4 cursor-pointer focus:outline-none border border-transparent focus:border-gray-800 focus:shadow-outline-gray bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 w-8 h-8 rounded flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" @click="sendMessage">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" @click="openCreateDialog" >
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <line x1="12" y1="5" x2="12" y2="19" />
                                 <line x1="5" y1="12" x2="19" y2="12" />
                             </svg>
+                            <!-- <div v-if="addInterview">
+                                <Modal />
+                            </div> -->
                         </div>
                     </div>
                 </div>
@@ -171,14 +175,15 @@
                             <td class="text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{{result.bio}}</td>
 
                             <td class="pr-8 relative">
+                                <div v-if="showBullets" class="opacity-0 fixed inset-0" @click="showrow"></div>
                                 <div v-if="rowId === result.interviewId && showBullets" class="dropdown-content mt-8 absolute left-0 -ml-12 shadow-md z-10 w-32">
                                     <ul class="bg-white dark:bg-gray-800 shadow rounded py-1">
                                         <li class="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-indigo-700 hover:text-white px-3 font-normal">Edit</li>
-                                        <li class="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-indigo-700 hover:text-white px-3 font-normal" @click="toggleModal">Delete</li>
+                                        <li class="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-indigo-700 hover:text-white px-3 font-normal" @click="toggleModal(result.interviewId)">Delete</li>
                                     </ul>
                                 </div>
                                 <button class="text-gray-500 rounded cursor-pointer border border-transparent focus:outline-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" @click="openSide(result.interviewId)" @focusout="showrow" class="icon icon-tabler icon-tabler-dots-vertical dropbtn" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" @click="openSide(result.interviewId)" class="icon icon-tabler icon-tabler-dots-vertical dropbtn" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" />
                                         <circle cx="12" cy="12" r="1" />
                                         <circle cx="12" cy="19" r="1" />
@@ -192,72 +197,75 @@
             </div>
         </div>
     </div>
-        <!-- DELETE MODAL -->
-            <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
-
-                        <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
-                        <div class="text-center p-5 flex-auto justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                            </svg>
-                                    <p class="text-xl font-bold py-4 ">Are you sure?</p>
-                                    <p class="text-sm text-gray-500 px-8">Do you really want to delete this interview?This process cannot be undone</p>
-                        </div>
-
-                    <!--footer-->
-                    <div class="p-3  mt-2 text-center space-x-4 md:block">
-                        <button class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100" v-on:click="toggleModal()">
-                            Cancel
-                        </button>
-                        <button class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600" v-on:click="toggleModal()">Delete</button>
-                    </div>
-
-                        </div>
-                </div>
-                <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            <!-- DELETE MODAL -->
+        <div v-if="showModal">
+            <DeleteModal
+            :rowId="rowId"
+            @changevalue='closemodal'
+            @refetchinterviews='fetchInterviews'
+            />
+        </div>
+        <div v-if="addInterview">
+            <Modal
+            @refetchinterviews='fetchInterviews'
+            @changingvalue='closemodal'
+            />
+        </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from '@vue/runtime-core'
 import { useFetchInterviews } from '../hooks/useFetchInterviews'
+import DeleteModal from './DeleteModal.vue'
+import Modal from './Modal.vue'
+
 import moment from 'moment'
 
 export default defineComponent({
   name: 'CompactTableWithActionsAndSelect',
-  //   props: {
-  //     result: {
-  //       type: [Object] as PropType<Interview>,
-  //       required: false
-  //     }
-  //   },
+  components: {
+    DeleteModal,
+    Modal
+  },
   setup () {
     const showBullets = ref(false)
     const showModal = ref(false)
+    const addInterview = ref(false)
     const rowId = ref('')
 
-    const toggleModal = () => {
+    const toggleModal = (id: string) => {
       showModal.value = !showModal.value
       showBullets.value = false
-      console.log('show bull value is', showBullets.value)
+      rowId.value = id
+      console.log('id is ', rowId.value)
     }
     const openSide = (id : string) => {
       showBullets.value = !showBullets.value
       rowId.value = id
     }
     const showrow = () => {
-      showBullets.value = !showBullets
+      showBullets.value = !showBullets.value
+      console.log('show bull value is', showBullets.value)
     }
-    const { result } = useFetchInterviews()
+    const openCreateDialog = () => {
+      console.log('i ve been opened')
+      addInterview.value = !addInterview.value
+      console.log('add interv value', addInterview.value)
+    }
+    const closemodal = () => {
+      showModal.value = false
+      addInterview.value = false
+    }
+    const { result, fetchInterviews } = useFetchInterviews()
 
     return {
       toggleModal,
       showModal,
+      closemodal,
+      openCreateDialog,
       result,
+      fetchInterviews,
       showrow,
+      addInterview,
       showBullets,
       openSide,
       rowId,
