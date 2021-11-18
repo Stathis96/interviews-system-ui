e4812ebcd8b2e1b2603b0f505ef21bfb43b86238
Stathis Ioannidis <56734272+Stathis96@users.noreply.github.com>
2021-11-18 16:47:30 +0200

    Interfaced created

---
+++ src/interfaces/Interview.ts
@@ -0,0 +1,20 @@
+export default interface Interview{
+  interviewId: string,
+  date: Date,
+  city: string,
+  area: string,
+  firstName: string,
+  lastName: string,
+  mobile: string,
+  age: number,
+  healthCertificate: boolean,
+  workPermit: boolean,
+  efetSeminars: boolean,
+  vaccinated: boolean,
+  doses : number,
+  shifts : number,
+  comments: string[],
+  toStore : string[],
+  result: string,
+  bio: string
+}
---
+++ src/interfaces/classes/InterviewInputData.ts
@@ -0,0 +1,19 @@
+export default interface InterviewInputData{
+  date: string,
+  city: string,
+  area: string,
+  firstName: string,
+  lastName: string,
+  mobile: string,
+  age: number,
+  healthCertificate: boolean,
+  workPermit: boolean,
+  efetSeminars: boolean,
+  vaccinated: boolean,
+  doses : number,
+  shifts : number,
+  comments: string[],
+  toStore : string[],
+  result: string,
+  bio: string
+}
